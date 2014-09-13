af7a809b4a7f815db417d0e0d0fb5cd7656122ae
Amrithamani <amrimani91@gmail.com>
2014-09-13 18:04:10 -0400

    basic html program

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
-<h1>Test new program P1 -updated !</h1>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Amritha Mani P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<style>
+
+			h1 {
+				text-align:center;
+			}
+
+			.container {
+				width:50%;
+				min-width:600px;
+				margin-bottom:50px;
+			}
+
+			img {
+				display:block;
+				margin:auto;
+				width:50%;
+			}
+
+		</style>
+
+</head>
+<body>
+
+
+       <div class="container">
+
+     		<h1>Amritha Mani <br> Dynamic Web Apps Portfolio</h1>
+
+    		<img src=>
+
+    		<h3>Project 2 Title</h3>
+    		View: [link]
+    		Github: [link]
+
+    		<h3>Project 3 Title</h3>
+     		View: [link]
+    		Github: [link]
+
+    		<h3>Project 4 Title</h3>
+    		View: [link]
+    		Github: [link]
+
+
+</div>
+
+</body>
+</html>
+
+
+
