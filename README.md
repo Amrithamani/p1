c5614fe1c2962eb4963e64218082d67a9c852e2b
Amrithamani <amrimani91@gmail.com>
2014-09-14 15:55:24 -0400

    some final changes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-9/13/2014
+# Dynamic web apps portfolio
+----------------------------
+
+## Live URL
+<http://p1.amrithamani.me>
+
+## Description
+A single page website using HTML and CSS.
+
+## Details for teaching team
+None
+
+The file `word-scrape.php` was used to generate the word list.
+
+## Outside code
+* Word list: http://www.paulnoll.com/Books/Clear-English/
+* Bootstrap: http://getbootstrap.com/
+* Bootstrap Theme: http://bootswatch.com/flatly/
--- index.php
+++ index.php
@@ -17,36 +17,34 @@
 
      		<h1>Amritha Mani <br> Dynamic Web Apps Portfolio</h1>
 
-    		<img src=>
+    		<img src=Amritha.jpg>
 
     		<h3>Project 2 Title</h3>
-    		<para>View: [link]</para>
-    		<para>Github: [link]</para>
+    		<p>View:<a href="http://p2.amrithamani.me">project 2</a>
+    		Github:<a href="https://github.com/Amrithamani/p2">Github url 2</a></p>
 
-    		<h3>Project 3 Title</h3>
-     		<para>View: [link]</para>
-    		<para>Github: [link]</para>
+			<h3>Project 3 Title</h3>
+			<p>View: <a href="http://p3.amrithamani.me">project 3</a>
+    		Github: <a href="https://github.com/Amrithamani/p3">Github url 3</a></p>
 
     		<h3>Project 4 Title</h3>
-    		<para>View: [link]</para>
-    		<para>Github: [link]</para>
-
-
+			<p>View: <a href="http://p4.amrithamani.me">project 4</a>
+    		Github: <a href="https://github.com/Amrithamani/p4">Github url 4</a> </p>
 
     		<h2>In 2+ sentences describe who you are and what you do.</h2>
-    		<p></p>
+    		<p>I am Indian national who recently moved to the US with my husband. Currently a home maker, I am aspiring to become a software professional. I spend time reading books from the local library, gardening and sewing.</p>
 
     		<h2>Why are you taking this course?</h2>
-    		<p></p>
+    		<p>I want to aquire knowledge on Web application development for my professional development. Having a bachelor's degree in electrical, I believe that this course would help me toward the pursuit of a master's degree in software engineering.</p>
 
     		<h2>What previous experience do you have with programming?</h2>
-    		<p></p>
+    		<p>I have learnt and did coursework in high-level langauges like Java and C/C++ during my under grad.</p>
 
     		<h2>Are you working on a Mac, PC, or some other platform?</h2>
-    		<p></p>
+    		<p>I am using PC for this course</p>
 
     		<h2>Anything else we should know?</h2>
-    		<p></p>
+    		<p>I am glad to have taken this course</p>
 
 
 </div>
