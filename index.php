e8b87f5459c58ed32c58221554911e557e23aa62
Amrithamani <amrimani91@gmail.com>
2014-09-14 13:32:50 -0400

    added some topics for questions

--- index.php
+++ index.php
@@ -32,6 +32,23 @@
     		<para>Github: [link]</para>
 
 
+
+    		<h2>In 2+ sentences describe who you are and what you do.</h2>
+    		<p></p>
+
+    		<h2>Why are you taking this course?</h2>
+    		<p></p>
+
+    		<h2>What previous experience do you have with programming?</h2>
+    		<p></p>
+
+    		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+    		<p></p>
+
+    		<h2>Anything else we should know?</h2>
+    		<p></p>
+
+
 </div>
 
 </body>
